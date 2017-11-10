58786bfbab8e2026308760dcc13f72e4385c0e70
Dasilvaj16 <Jeremy.daSilvaMachado@rpn.ch>
2017-11-10 11:10:03 +0100

    Create the main source for my skeleton

--- Program.cs
+++ Program.cs
@@ -10,6 +10,59 @@ namespace Calculatrice
     {
         static void Main(string[] args)
         {
+            double dblVal1, dblVal2, dblRep = 0;
+            bool isOpInCorrect = true;
+
+            Console.WriteLine("Calculatrice");
+            Console.WriteLine("------------");
+
+            // Saisie des opérandes, avec vérification de ces dernières
+            Console.Write("Saisir la 1ère valeur : ");
+            while (!double.TryParse(Console.ReadLine(), out dblVal1))
+            {
+                Console.Write("Saisir a nouveau la 1ère valeur : ");
+            }
+
+            Console.Write("Saisir la 2ème valeur : ");
+            while (!double.TryParse(Console.ReadLine(), out dblVal2))
+            {
+                Console.Write("Saisir a nouveau la 2ème valeur : ");
+            }
+
+            // Siaisie du choix de l'opérateur
+            Console.Write("Opération a choix [+ - * / ^] : ");
+            while (isOpInCorrect)
+            {
+                string strOp = Console.ReadLine();
+                isOpInCorrect = false;
+                switch (strOp)
+                {
+                    default:
+                        Console.WriteLine("Opération non reconnue par le système");
+                        Console.Write("Veulliez saisir à nouveau l'opérateur [+ - * / ^] : ");
+                        isOpInCorrect = true;
+                        break;
+                    case "+":
+                        dblRep = 0;
+                        break;
+                    case "-":
+                        dblRep = 0;
+                        break;
+                    case "*":
+                        dblRep = 0;
+                        break;
+                    case "/":
+                        dblRep = 0;
+                        break;
+                    case "^":
+                        dblRep = 0;
+                        break;
+
+                }
+            }
+            Console.Write(dblVal1.ToString(), dblVal2.ToString());
+
+            Console.ReadKey();
         }
     }
 }
